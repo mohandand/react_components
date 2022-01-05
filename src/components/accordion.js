87a64ae9dd9e46584110a4acb28d791e763bd742
mohandand <mohandand@gmail.com>
2022-01-05 10:11:50 -0800

    updated code

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -2,25 +2,22 @@ import React, {useState} from 'react';
 
 const Accordion = ({questions}) => {
 
-    const [symbol, setSymbol] = useState("+");
+    const [isActive, setIsActive] = useState("true");
 
-    const[answerstyle, setAnswerstyle] = useState("hideText")
 
-    const changeStyle = () => {
-        setSymbol("-");
-        setAnswerstyle("show");
-    }
 
     let element = questions.map((item,index)=>{
         return(
             <div className='element' key={index}>
                <div className="question">
                  <h3>{item.question}</h3>
-                 <button onClick={changeStyle}>{symbol}</button>
+                 <button onClick={() => setIsActive(!isActive)}>{isActive ? '-' : '+'}</button>
                </div>
-               <div className={answerstyle}>
+               {isActive &&
+               <div className="answer">
                     <p>{item.answer}</p>
                </div>
+               }
            </div>
         )
     })
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Modal = () => {
+
+    const [modalstyle ,setModalstyle] = useState("hideText")
+
+    const changeStyle = () => {
+        setModalstyle("show");
+    }
+
     return(
         <div className="modalcontainer">
-            <button>Open Modal</button>
+            <button onClick={changeStyle}>Open Modal</button>
+            <div className={modalstyle}>
+                <div class="modal-container">
+                    <h2> Modal Box</h2>
+                    <h3> Welcome to React Components</h3>
+                </div>
+            </div>
         </div>
     )
 }
