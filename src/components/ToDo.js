b3b368ddbbcf86d37a5915ccfefcc0f934a6e8c8
mohandand <mohandand@gmail.com>
2022-01-07 16:37:07 -0800

    updated code

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
 
 const ToDo = () => {
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const getTask= (event) => {
+        setTask(event.target.value);
+    }
+    const pushTask = (event) => {
+        let newtask = tasks;
+        newtask.push(task);
+        setTasks(newtask);
+        console.log(tasks);
+    }
+    
+
+    let taskslist = tasks.map((tas,index) => {
+                return <div key={index}>{tas}<button>X</button></div>
+    })
+
     return(
         <div className="todocontainer">
             <div className="addtask">
-                <input type="text"/>
-                <button>Add</button>
+                <input type="text" onChange={getTask}/>
+                <button onClick={pushTask}>Add</button>
             </div>
             <div className="displaytasks">
+                {taskslist}
             </div>
         </div>
     )
